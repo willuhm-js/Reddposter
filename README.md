6184ba13545435ea4761a5981bde4e2991fa74b0
William Faircloth <>
2021-05-11 18:57:29 +0000

    basic

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reddposter
+Bot that grabs content from Reddit, repackages it, and reposts it on Instagram.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Load modules we will need
+const { Canvas, resolveImage } = require('canvas-constructor'), // For repackaging
+{ registerFont } = require('canvas')
+redditFetch = require('reddit-fetch'); // For getting post
+
