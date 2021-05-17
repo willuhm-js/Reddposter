23bbeaf70a10023e24a5f63e82f7f0e3b7c10ed5
William Faircloth <>
2021-05-17 20:08:13 +0000

    Add replit compatibility

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const fs = require("fs");
 let previousPost = { /* title */ }
 
 console.log(`The bot has started with an interval of ${interval} milliseconds.`);
+
 setInterval(() => {
   let previous = +fs.readFileSync(`${__dirname}/timeManager.txt`)
   if (Date.now() > (previous + interval)) {
@@ -22,3 +23,21 @@ setInterval(() => {
     }).catch(e => console.error(e));
   }
 }, 1000);
+
+/* If running on replit.com, use this code instead of the above code
+const Database = require("@replit/database")
+const db = new Database()
+
+setInterval(async () => {
+  let previous = await db.get("previous")
+  if (Date.now() > (previous + interval)) {
+    await db.set("previous", Date.now())
+    fetchPost(previousPost).then(async post => {
+      previousPost.text = post.text;
+      let tweet = await repackagePost(post);
+      await publishPost(tweet);
+      console.log(`New post made [${Date.now()}]`)
+    }).catch(e => console.error(e));
+  }
+}, 1000);
+*/
